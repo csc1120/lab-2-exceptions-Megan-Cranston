63c1918c750c09ea8039e22ad77f795ce183c3e3
cranstonm <cranstonm@msoe.edu>
2024-09-12 11:44:59 -0500

    Checkstyle Updated

---
+++ src/cranstonm/Driver.java
@@ -0,0 +1,15 @@
+/*
+ * Course: CSC1020
+ * Lab 2 - Exceptions
+ * Die class
+ * Name: Megan Cranston
+ * Last Updated: 9/12/2024
+ */
+package cranstonm;
+
+/**
+ * Class Driver
+ */
+public class Driver {
+
+}
