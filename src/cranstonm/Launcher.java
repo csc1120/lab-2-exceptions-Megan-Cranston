5bed9370d7753436f3e36bd976d10aeed2e5039d
Megan-Cranston <cranstonm@msoe.edu>
2024-09-17 15:55:05 -0500

    Commit 10 CheckStyle

--- src/cranstonm/Launcher.java
+++ src/cranstonm/Launcher.java
@@ -1,5 +1,15 @@
+/**
+ * Course: CSC1020
+ * Lab 2 - Exceptions
+ * Launcher class
+ * Name: Megan Cranston
+ * Last Updated: 9/17/2024
+ */
 package cranstonm;
 
+/**
+ * Launcher Class
+ */
 public class Launcher {
     public static void main(String[] args) {
         Driver.main(args);
